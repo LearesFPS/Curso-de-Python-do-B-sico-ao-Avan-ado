76a800de2b25613b4f65c00d35a43da18d1808f2
Learesfps <107267494+LearesFPS@users.noreply.github.com>
2022-06-10 17:24:38 +0000

    AULA 02/ O Comando Print

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# isso é um comentário
-# Funçao "Print" é um comando para fazer a mensagem aparecer na tela do usuário
-print("Linha 1")
-# O interpretador do python começa a ler o código da esquerda para direita de cima para baixo
-
-"""
-Comentário de Linhas duplas 3 áspas quando se abre fecha com 3 áspas duplas.
-"""
+#print('João', 'Maria', end= '.') #é usado ao final da frase
+#Ex1 Faça o programa de Gerador de CPF: 045.855.456-02
+print('045', '855', '456', sep= '.', end= '-' )
+print('02')
