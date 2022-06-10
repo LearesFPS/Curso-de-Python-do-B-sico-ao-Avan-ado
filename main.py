a5b69c7d679c2c3bfe5de46e7345f31b042e9f70
Learesfps <107267494+LearesFPS@users.noreply.github.com>
2022-06-10 16:00:59 +0000

    Aula 01/ comentários de código em python

---
+++ main.py
@@ -0,0 +1,8 @@
+# isso é um comentário
+# Funçao "Print" é um comando para fazer a mensagem aparecer na tela do usuário
+print("Linha 1")
+# O interpretador do python começa a ler o código da esquerda para direita de cima para baixo
+
+"""
+Comentário de Linhas duplas 3 áspas quando se abre fecha com 3 áspas duplas.
+"""
